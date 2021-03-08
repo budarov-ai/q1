4884b4a35d458826590e0ba662fec86607672f24
a <budarov0@gmail.com>
2021-03-09 00:17:40 +0700

    ydalil test fle i dopisal code

--- q6.py
+++ q6.py
@@ -2,10 +2,13 @@ start = int(input('how much? :'))
 finish = int(input('how much? :'))
 day = 1
 day_range = start + (day / 100 * 10)
-while day_range <= 3:
+while day_range <= finish:
     day += 1
-    day_range += start
+    day_range *= 1.1
     print(f'eshe ne dobejal :( %.d' % day_range)
+    if day_range >=finish:
+        print("условие выполнено :", day_range)
+
 
 
 
