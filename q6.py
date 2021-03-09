18fe173523f033035e1f9025c459abf515bb63bb
a <budarov0@gmail.com>
2021-03-09 07:19:38 +0700

    update code

--- q6.py
+++ q6.py
@@ -7,11 +7,14 @@ day_range = start + (day / 100 * 10)
 while day_range <= finish:
     day += 1
     day_range *= 1.1
+    if day > 6:
+        print('Условие не выполнено, не более 6 дней.')
 #status  = '+' if day_range > finish else '-'
 #print(f'ok:{status} %.d' % day_range)
-    print(day, f'eshe ne dobejal :( %.d' % day_range)
+    print(f'den', day, f'eshe ne dobejal :( %.d' % day_range)
     if day_range >=finish:
         print("условие выполнено :", day_range)
+
 
 
 
